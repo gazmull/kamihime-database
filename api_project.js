afd50f2057d1e38c0b2e2e0ff149612fc9d1e589
Euni <vescalaw@gmail.com>
2018-12-30 08:32:25 +0800

    Adjust GET /list example

--- api_project.js
+++ api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-12-29T23:29:29.137Z",
+    "time": "2018-12-30T00:31:17.393Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
