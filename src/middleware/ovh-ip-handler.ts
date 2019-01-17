5ccd5e82f0ce5edb79e4b7cd9c28339d3fd1ecaf
Euni <vescalaw@gmail.com>
2019-01-18 05:20:18 +0800

    Fix: typo on next

--- src/middleware/ovh-ip-handler.ts
+++ src/middleware/ovh-ip-handler.ts
@@ -9,7 +9,7 @@ export default function ovhIpHandler (): RequestHandler {
       : req.ip;
 
     // @ts-ignore
-    if ([ '213.32.4.0/24', '54.39.240.0/24', '144.217.9.0/24' ].includes(ip)) return next;
+    if ([ '213.32.4.0/24', '54.39.240.0/24', '144.217.9.0/24' ].includes(ip)) return next();
 
     if (!ip) {
       res
