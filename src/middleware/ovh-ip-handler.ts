2d6f17954809770e5fd38da8c62874173fd761a6
Euni <vescalaw@gmail.com>
2019-01-18 05:45:25 +0800

    [skip travis] Debug

--- src/middleware/ovh-ip-handler.ts
+++ src/middleware/ovh-ip-handler.ts
@@ -4,14 +4,13 @@ export default function ovhIpHandler (): RequestHandler {
   return (req, res, next) => {
     // See: https://docs.ovh.com/fr/ssl-gateway/utiliser-le-ssl-gateway/
     const forwarded = req.headers['X-Forwarded-For'];
+    console.log(forwarded, req.ip); // tslint:disable-line
     const forwardedIp = Array.isArray(forwarded) ? forwarded[0] : forwarded;
     const isProxy = [ '213\.32\.4\.\d{1,3}', '54\.39\.240\.\d{1,3}', '144\.217\.9\.\d{1,3}' ]
       .some(el => new RegExp(el).test(req.ip));
     const ip = process.env.NODE_ENV === 'production'
       ? req.headers['X-Remote-Ip'] || forwardedIp
       : req.ip;
-
-    console.log(forwarded, ip, req.ip); // tslint:disable-line
 
     if (isProxy) return next();
     if (!ip) {
