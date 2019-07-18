b0b3348d342d5fe0940b42b4636cd3a099966ee3
Euni <vescalaw@gmail.com>
2019-07-18 18:23:45 +0800

    Feat(Browse): Bring Back Character Preview on Hover

--- src/static/js/browse.ts
+++ src/static/js/browse.ts
@@ -9,6 +9,35 @@ $(() => {
     newBtn.addClass('active');
   });
 
+  let imgTimeout = null;
+  $('#browse-parent li a')
+    .on('mouseenter', function () {
+      if (!this.textContent) return;
+      if (imgTimeout) clearTimeout(imgTimeout);
+
+      imgTimeout = setTimeout(() => {
+        const name = this.textContent.replace(/'/g, '\\$&');
+        const me = $(this);
+
+        if (!me.data('bs.tooltip'))
+          me
+            .tooltip({
+              animation: false,
+              html: true,
+              title: `<img src="/img/wiki/close/${name} Close.png" />`,
+              placement: "auto",
+              trigger: 'manual'
+            })
+        if ($('.modal-open').length) return; // Fix for mobile (popping up when only pressed)
+
+        return me.tooltip('show');
+      }, 300);
+    })
+    .on('mouseleave', function () {
+      if (imgTimeout) clearTimeout(imgTimeout);
+      $(this).tooltip('hide');
+    });
+
   const target = $('.browse-panel .btn').first().addClass('active').data('target');
 
   $(target).collapse('show');
