680b4aa38d2f7be041c74f8a82b7a5ae5b95b92d
Euni <vescalaw@gmail.com>
2020-01-08 01:13:47 +0800

    Fix(player/scenario): .wav format fallback

--- src/static/js/player/scenario.ts
+++ src/static/js/player/scenario.ts
@@ -1,6 +1,6 @@
 $(async () => {
   const images = files.filter((v, i, arr) => v && v.endsWith('.jpg') && arr.indexOf(v) === i);
-  const audios = files.filter((v, i, arr) => v && !v.startsWith('bgm') && v.endsWith('.mp3') && arr.indexOf(v) === i);
+  const audios = files.filter((v, i, arr) => v && !v.startsWith('bgm') && (v.endsWith('.mp3') || v.endsWith('.wav')) && arr.indexOf(v) === i);
   const bgm = files.find(i => i.startsWith('bgm'));
 
   const talkVal = parseInt($('#text').attr('data'));
